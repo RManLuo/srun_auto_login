b5a83464830d9ead942956501a01ac256db79cb0
Raymond LUO <34737002+RManOfCN@users.noreply.github.com>
2019-09-17 01:22:09 +0800

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+<h1 align="center">Welcome to srun_auto_login 👋</h1>
+<p>
+  <a href="https://mit-license.org/">
+    <img alt="License: MIT License" src="https://img.shields.io/badge/License-MIT License-yellow.svg" target="_blank" />
+  </a>
+</p>
+
+> 深澜校园网自动后台常驻保证在线，支持windows、linux、mac，妈妈再也不用担心电脑被挤掉线了。
+
+## Requirements
+
+```sh
+selenium >= 3.141.0
+```
+
+## Usage
+在config.py文件中输入您的账户和密码，然后愉快的开始运行吧。
+
+### WIndows后台运行
+```sh
+pythonw auto_login.py
+```
+### Mac and Linux后台运行
+```sh
+nohup python3 auto_login.py 2>&1 &
+```
+## 通过bat注册windows开机自启
+1.首先，需要新建一个start.bat文件（用来运行脚本, 写完之后保存
+```shell script
+pythonw /绝对路径/auto_login.py
+```
+2.点击开始--所有程序--启动--右击--打开，将已经保存的.bat文件复制到该目录（C:\Users\Administrator\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup）下，可能杀毒软件会阻止，选择允许，然后重启电脑即可。
+## Author
+
+👤 **Raymond Luo**
+
+* Github: [@RManOfCN](https://github.com/RManOfCN)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+Copyright © 2019 [Raymond Luo](https://github.com/RManOfCN).<br />
+This project is [MIT License](https://mit-license.org/) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ auto_login.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python3
+# -*-coding:utf-8 -*-
+# Reference:**********************************************
+# @Time     : 2019/9/16 21:45
+# @Author   : Raymond Luo
+# @File     : auto_login.py
+# @User     : luoli
+# @Software: PyCharm
+# Reference:**********************************************
+import os
+import config
+import time
+import logging
+import subprocess
+import platform
+import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class AutoLogin(object):
+    def __init__(self, username, password, wifi):
+        '''
+        构建登录信息
+        :param username: 用户名
+        :param password: 密码
+        :param wifi: wifi模式
+        '''
+        self.username = username
+        self.password = password
+        self.wifi = wifi
+        self.login_gateway = "http://10.0.10.66/srun_portal_pc.php" if wifi == "utsz" else "http://10.248.98.2/srun_portal_pc?ac_id=1&theme=basic2"  # 设置网关
+
+        if config.log:  # 记录log
+            self.logger = logging.getLogger(__name__)
+            self.logger.setLevel(level=logging.INFO)
+            handler = logging.FileHandler(os.path.join(config.log_path, "log.txt"))
+            handler.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            handler.setFormatter(formatter)
+            self.logger.addHandler(handler)
+            if config.debug:  # debug 在控制台输出
+                console = logging.StreamHandler()
+                console.setLevel(logging.INFO)
+                self.logger.addHandler(console)
+
+        if wifi == 'hitsz':  # 设置Chrome
+            self.chrome_options = Options()
+            self.chrome_options.add_argument('--headless')
+            self.chrome_options.add_argument('--disable-gpu')
+            operating_system = platform.platform()
+            # 根据操作系统获取driver version 77
+            if 'Windows' in operating_system:
+                self.path = 'webdriver/chromedriver_win32.exe'
+            elif 'Mac' in operating_system:
+                self.path = 'webdriver/chromedriver_mac64'
+            elif 'Linux' in operating_system:
+                self.path = 'webdriver/chromedriver_linux64'
+            else:
+                self.logger.warning("Not support this system :{}".format(operating_system))
+                raise Exception("Not support this system :{}".format(operating_system))
+
+    def _check_network(self):
+        '''
+        检查网络是否畅通
+        :return: Ture为畅通，False为不畅通。
+        '''
+        exit_code = subprocess.call("ping www.baidu.com", shell=True)
+        if exit_code:
+            return False
+        else:
+            return True
+
+    def _login_utsz(self):
+        '''
+        登录utsz
+        :return:
+        '''
+        header = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+            'Accept': '*/*',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+        }
+        data = {'action': 'login', 'username': self.username, 'password': self.password, "ac_id": 1}
+        req = requests.post(self.login_gateway, headers=header, data=data)
+        return
+
+    def _login_hitsz(self):
+        '''
+        登录hitsz
+        :return:
+        '''
+        driver = webdriver.Chrome(executable_path=self.path, chrome_options=self.chrome_options)
+        driver.set_page_load_timeout(10)
+        driver.set_script_timeout(10)  # 超时
+        try:
+            driver.get(self.login_gateway)
+        except:
+            self.logger.warning("Get gatway out of time....try again soon")
+            return
+        time.sleep(2)
+        username_box = driver.find_element_by_xpath('//*[@id="username"]')
+        password_box = driver.find_element_by_xpath('//*[@id="password"]')
+        username_box.send_keys(self.username)
+        password_box.send_keys(self.password)
+        driver.find_element_by_xpath('//*[@id="login"]').click()  # 登录
+        time.sleep(3)  # 等那个sb回调json
+        driver.quit()
+        return
+
+    def _login(self):
+        '''
+        登录网络
+        :return: 成功返回True 失败返回 False
+        '''
+        i = 1
+        while i <= config.retry:
+            self.logger.info("Start trying times: {}".format(i))
+            if self.wifi == "utsz":
+                self._login_utsz()
+            else:
+                self._login_hitsz()
+            status = self._check_network()
+            if status:
+                self.logger.info("Loging success")
+                return True
+            else:
+                i += 1
+                time.sleep(10)  # 等10秒再尝试
+        if i > config.retry:
+            self.logger.warning("Out of trying times")
+            raise Exception("Out of trying times")
+
+    def start(self):
+        start_time = time.time()
+        self.logger.info("Start watching network status")
+        first = True
+        while True:
+            if time.time() - start_time > config.check_time or first:  # check是否掉线
+                first = False  # 只有第一次
+                start_time = time.time()  # 更新计时器
+                self.logger.info("Checking network")
+                if self._check_network():
+                    self.logger.info("Network is good")
+                else:
+                    self.logger.info("Network is disconnected. Try logging now.")
+                    self._login()  # 重新登录
+
+
+if __name__ == "__main__":
+    login = AutoLogin(config.username, config.passowrd, config.wifi)
+    login.start()
